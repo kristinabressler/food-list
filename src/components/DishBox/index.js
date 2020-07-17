aed19a5f47a049894000f1d85e8dca77b02d44c3
kikibres <kristinabressler@yahoo.com>
2020-07-17 11:49:43 -0400

    updated app

--- src/components/DishBox/index.js
+++ src/components/DishBox/index.js
@@ -11,9 +11,13 @@ export default class DishBox extends Component {
     console.log(this.props.dish);
     return (
       <div className="dish-box">
-        <div className="food-title"><h2>{this.props.dish}</h2></div>
-        <p>{this.state.ingredients.join(", ")}</p>
-        <button onClick={this.props.clickToDelete}>Delete</button>
+        <div className="left-flex">
+          <div className="food-title"><h2>{this.props.dish}</h2></div>
+          <p>{this.state.ingredients.join(", ")}</p>
+        </div>
+        <div className="right-flex">
+          <button type="button" className="btn btn-danger" onClick={this.props.clickToDelete}>Delete</button>
+        </div>
       </div>
     )
   }
